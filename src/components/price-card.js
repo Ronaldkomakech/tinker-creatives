8fd842298ea84366f3c978dfed091dd3cd8d3995
Ronaldkomakech <komakechronald10@gmail.com>
2021-07-11 10:15:40 +0300

    updated some sections of the website

--- src/components/price-card.js
+++ src/components/price-card.js
@@ -30,8 +30,24 @@ export default function PriceCard({
         </Box>
         <List items={points} childStyle={styles.listItem}/>
         <Text className="package__price" sx={styles.price}>
-          {priceWithUnit}  
+          {priceWithUnit} 
+          <span>Monthly</span> 
         </Text>
+        <Box sx={styles.buttonGroup}>
+          <Button variant="primary" aria-label={buttonText}>
+            {buttonText}
+          </Button>
+          {anotherOption && (
+            <Button
+              variant="textButton"
+              className="free__trial"
+              aria-label={anotherOption}
+              sx={{ color: 'black'}}>
+                {anotherOption}
+              </Button>
+          )}
+
+        </Box>
       </Box>
 
     </Card>
